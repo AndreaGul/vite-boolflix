c5f79354b32dfdc35391f7fd8cec881f73703a9c
AndreaGul <andreagulino99@gmail.com>
2023-12-16 23:33:21 +0100

    feat:aggiunta serie tv alla ricerca

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -22,39 +22,78 @@ export default {
   },
 
   methods: {
-    searchFilm(event){
-      axios.get(this.store.apiUrl,{ 
+     searchFilmESeries(event){
+      axios.get(this.store.apiUrlFilm,{ 
+         params: {
+          api_key: this.store.apiKey,
+           query: event,
+         }
+      } ).then((response) => {
+         this.store.searchedFilm = response.data.results
+         console.log('Film', response.data.results)
+       })
+
+       axios.get(this.store.apiUrlSeries, {
         params: {
           api_key: this.store.apiKey,
           query: event,
         }
-      } ).then((response) => {
-        this.store.SearchedFilm = response.data.results
-        console.log( response.data.results)
-      })
-    },
+       }).then(response => {
+        this.store.searchedSeries = response.data.results
+        console.log('Series',response.data.results)
+       })
 
+     },
 
-    
+    // individualSearch(apiUrl,apiKey,query){
+    //   axios.get(apiUrl,{ 
+    //     params: {
+    //       api_key: apiKey,
+    //       query: query,
+    //     }
+    //   } ).then((response) => {
+    //     if(response.data.results){
+    //       console.log( response.data.results)
+    //        return response.data.results
+    //     }
+    //   })
+    // },
+    // searchFilm(event){
+    //   this.store.SearchedFilm = this.individualSearch(this.store.apiUrl,this.store.apiKey,event)
+      
+    //   console.log(this.store.SearchedFilm)
+    // }
   },
 
-  
+
+ 
   
 };
 </script>
 
 <template>
   <main>
-    <AppMainSearch @search="searchFilm"/>
+    <AppMainSearch @search="searchFilmESeries"/>
     <ul>
-    <li v-for="film in this.store.SearchedFilm">
-      <AppMainFilmList 
-      
-      :title="film.title"
-      :titleOriginal=" film.original_title"
-      :lang="film.original_language"
-      :vote="film.vote_average"/>
-    </li>
+      <li v-if="this.store.searchedFilm.length"><h2>Film</h2></li>
+      <li v-for="film in this.store.searchedFilm">
+        <AppMainFilmList 
+        
+        :title="film.title"
+        :titleOriginal=" film.original_title"
+        :lang="film.original_language"
+        :vote="film.vote_average"/>
+      </li>
+
+      <li v-if="this.store.searchedSeries.length"><h2>Series</h2></li>
+      <li v-for="series in this.store.searchedSeries">
+        <AppMainFilmList 
+        
+        :title="series.name"
+        :titleOriginal=" series.original_name"
+        :lang="series.original_language"
+        :vote="series.vote_average"/>
+      </li>
       
     </ul>
     
--- src/store.js
+++ src/store.js
@@ -2,8 +2,10 @@ import {reactive} from 'vue';
 
 export const store= reactive ({
     searchKey: '',
-    SearchedFilm: [],
-    apiUrl: 'https://api.themoviedb.org/3/search/movie',
+    searchedFilm: [],
+    searchedSeries: [],
+    apiUrlFilm: 'https://api.themoviedb.org/3/search/movie',
+    apiUrlSeries: 'https://api.themoviedb.org/3/search/tv',
     apiKey: '7de02f08dfce896da1642ad84ebce808',
 
 })
