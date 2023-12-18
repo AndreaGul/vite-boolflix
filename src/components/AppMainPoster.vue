eb609dada9402a3d23ae9630756562fe3ff11d86
AndreaGul <andreagulino99@gmail.com>
2023-12-18 23:10:35 +0100

    feat:api url immagini spostata nello store js

--- src/components/AppMainPoster.vue
+++ src/components/AppMainPoster.vue
@@ -1,43 +1,25 @@
 <script>
-
-// import { store } from '../store';
-
+import { store } from '../store';
 
 export default {
   name: 'AppMainPoster',
-
-  data(){
-    return{
-        apiImgUrl: 'https://image.tmdb.org/t/p/w342',
-        
-    }
+  data() {
+    return {
+      store,
+    };
   },
 
-//   components: {
-//     store,
-//   },
-
-  props: ['locandina','name'],
-  
-  
- 
-
-  };
-
-
-
-
-
+  props: ['locandina', 'name'],
+};
 </script>
 
 <template>
-        
-         <img v-if="this.locandina !== null" :src="this.apiImgUrl + this.locandina" :alt="name">
-           <div v-else>Nessuna locandina</div>
-         
-    
-       
+  <img
+    v-if="this.locandina !== null"
+    :src="this.store.apiImgUrl + this.locandina"
+    :alt="name"
+  />
+  <div v-else>Nessuna locandina</div>
 </template>
 
 <style scoped lang="scss"></style>
-
--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ export const store = reactive({
   searchKey: '',
   searchedFilm: [],
   searchedSeries: [],
+  apiImgUrl: 'https://image.tmdb.org/t/p/w342',
   apiUrlFilm: 'https://api.themoviedb.org/3/search/movie',
   apiUrlSeries: 'https://api.themoviedb.org/3/search/tv',
   apiKey: '7de02f08dfce896da1642ad84ebce808',
